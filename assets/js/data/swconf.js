edd61429851f877b367b7f9fde960d0178e65842
zkoppert <zkoppert@github.com>
2026-03-28 00:21:11 +0000

    Deploying to gh-pages from @ zkoppert/zkoppert.github.io@3e32b334a44ba0f7e208afe3d9132cfa16f2523f 🚀

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1773467022',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1774657267',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
